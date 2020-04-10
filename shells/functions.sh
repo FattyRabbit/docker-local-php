e4126d90614e7a16ecadd6f647cf262efe70e22f
李國永 <kugyon_i@012grp.co.jp>
2020-04-10 18:43:55 +0900

    no message

--- shells/functions.sh
+++ shells/functions.sh
@@ -4,13 +4,13 @@
 function service_exists() {
     local n=$1
     if [[ $(systemctl list-units --all -t service --full --no-legend "$n.service" | cut -f1 -d' ') == $n.service ]]; then
-        return 0
-    else
         return 1
+    else
+        return 0
     fi
 }
 
-function convertTemplate() {
+function convert_template() {
   if [ ! -f $1 ]; then
     echo "Usage:\n$ convertTemplate template.tpl > target.txt"
     exit 1
